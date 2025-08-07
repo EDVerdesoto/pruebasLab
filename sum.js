c1c76f3e4502c0b238a126b32e1acab4ad2916cf
Edison Damian Verdesoto Segovia <132785383+EDVerdesoto@users.noreply.github.com>
2025-08-07 12:47:11 -0500

    Creacion de nuevas funciones y sus pruebas respectivas.

--- sum.js
+++ sum.js
@@ -2,4 +2,22 @@ function suma(a, b) {
     return a + b;
 }   
 
-module.exports = suma;
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    }       
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+module.exports = {
+    suma,
+    factorial,
+    fibonacci
+};
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,31 @@
+const { describe } = require('yargs');
 const suma = require('./sum');
+const { factorial, fibonacci } = require('./sum');
 
 test('Suma de 1 + 2 debe ser 4', () => {
     expect(suma(1, 2)).toBe(3);
+});
+
+describe('Factorial', () => {
+    test('Factorial de 0 debe ser 1', () => {
+        expect(factorial(0)).toBe(1);
+    }); 
+    test('Factorial de 1 debe ser 1', () => {
+        expect(factorial(1)).toBe(1);
+    });
+    test('Factorial de 5 debe ser 120', () => {
+        expect(factorial(5)).toBe(120);
+    });    
+});
+
+describe('Fibonacci', () => {
+    test('Fibonacci de 0 debe ser 0', () => {
+        expect(fibonacci(0)).toBe(0);
+    });
+    test('Fibonacci de 1 debe ser 1', () => {
+        expect(fibonacci(1)).toBe(1);
+    });
+    test('Fibonacci de 5 debe ser 5', () => {
+        expect(fibonacci(5)).toBe(5);
+    });
 });
