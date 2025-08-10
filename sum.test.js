ca804d08e226fbf64065e6485c81fe3cd6744bc1
Edison Damian Verdesoto Segovia <132785383+EDVerdesoto@users.noreply.github.com>
2025-08-09 23:25:11 -0500

    prueba arreglada

--- sum.test.js
+++ sum.test.js
@@ -10,7 +10,7 @@ describe('Factorial', () => {
         expect(factorial(0)).toBe(1);
     }); 
     test('Factorial de 1 debe ser 1', () => {
-        expect(factorial(4)).toBe(1);
+        expect(factorial(1)).toBe(1);
     });
     test('Factorial de 5 debe ser 120', () => {
         expect(factorial(5)).toBe(120);
